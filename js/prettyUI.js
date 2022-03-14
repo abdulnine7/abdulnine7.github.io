36f8474222934fa412be7e683f31e244cae3714f
abdulnine7 <me@abdul.co.in>
2022-03-14 09:28:10 +0530

    info text updated

--- js/prettyUI.js
+++ js/prettyUI.js
@@ -13,13 +13,13 @@ function flipWindow() {
   $("#card").flip('toggle');
   if($('.flip-img').hasClass('animate')) { $('.flip-img').removeClass('animate'); } 
   
-  if($(".front").css('z-index') == 1){
-    $('.flip-text')[0].innerHTML = "CLI";
-    document.title = 'Abdul\s GUI';
-  } else {
-    $('.flip-text')[0].innerHTML = "GUI";
-    document.title = 'Abdul\'s CLI - Terminal';
-  }
+  // if($(".front").css('z-index') == 1){
+  //   $('.flip-text')[0].innerHTML = "CLI";
+  //   document.title = 'Abdul\s GUI';
+  // } else {
+  //   $('.flip-text')[0].innerHTML = "GUI";
+  //   document.title = 'Abdul\'s CLI - Terminal';
+  // }
 
   $('.flip-img').addClass('animate');
 }
